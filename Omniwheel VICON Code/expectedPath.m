19f9e61db6ebdc8de53d21b89b9afcc1f374b45d
dyerbm <dyerb@uoguelph.ca>
2019-10-22 13:59:42 -0400

    gain factor tests + rose test

--- Omniwheel VICON Code/expectedPath.m
+++ Omniwheel VICON Code/expectedPath.m
@@ -11,19 +11,19 @@ function [xD, yD, thetaD] = expectedPath(time)
     
     
     %% Do a circle
-    radius=800;
-    circleTime = 10;
-    yD=radius*sin(2*pi/circleTime*time);
-    xD=radius*cos(2*pi/circleTime*time)-radius;
-    thetaD=atan(1)-pi/4;
+%     radius=800;
+%     circleTime = 10;
+%     yD=radius*sin(2*pi/circleTime*time);
+%     xD=radius*cos(2*pi/circleTime*time)-radius;
+%     thetaD=atan(1)-pi/4;
 
 
     %% Rose Curve Path
-%     circleTime=5;
-%     n=4; %must be an even number (2*n=number of pedals)
-%     a=100; %radius of rose curve
-%     yD=a*cos(n*2*pi/circleTime*time)*sin(2*pi/circleTime*time);
-%     xD=a*cos(n*2*pi/circleTime*time)*cos(2*pi/circleTime*time);
-%     thetaD=atan(1)-pi/4;
+    circleTime=10;
+    n=2; %must be an even number (2*n=number of pedals)
+    a=300; %radius of rose curve
+    yD=a*cos(n*2*pi/circleTime*time)*sin(2*pi/circleTime*time);
+    xD=a*cos(n*2*pi/circleTime*time)*cos(2*pi/circleTime*time);
+    thetaD=atan(1)-pi/4;
     
 end
