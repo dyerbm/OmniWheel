37ae5c00faa51df979b0038732843df312204d8e
dyerbm <dyerb@uoguelph.ca>
2019-12-13 15:40:03 -0500

    File cleaning and data restrucutre

--- Omniwheel VICON Code/cubic.m
+++
@@ -1,8 +0,0 @@
-function [xD, yD, thetaD] = cubic(time)
-    
-    yD = time*200;
-    %xD = time*200;
-    xD = 0;
-    thetaD = atan(1)-pi/4;
-
-end
--- Simulation/Variable_Finding.m
+++ Simulation/Variable_Finding.m
@@ -39,7 +39,7 @@ clear opts
 Input=[];
 Output=[];
 
-for i=21:2:size(data,1)-100
+for i=21:5:size(data,1)-100
     
     vector_ca1 = [(data.rb4x(i) - data.rb5x(i))/1000, (data.rb4y(i) - data.rb5y(i))/1000];
     theta1 = atan(vector_ca1(2)/vector_ca1(1));
@@ -47,7 +47,7 @@ for i=21:2:size(data,1)-100
     vector_ca2 = [(data.rb4x(i+1) - data.rb5x(i+1))/1000, (data.rb4y(i+1) - data.rb5y(i+1))/1000];
     theta2 = atan(vector_ca2(2)/vector_ca2(1));
     
-    pwm=[0,data.u2(i),0,-data.u4(i);data.u1(i),0,-data.u3(i),0;data.u1(i),data.u2(i),data.u3(i),data.u4(i)];
+    pwm=[data.u2(i)-data.u4(i),0,0;0,data.u1(i)-data.u3(i),0;0,0,data.u1(i)+data.u2(i)+data.u3(i)+data.u4(i)];
     xk=[data.rb5x(i)/1000,data.rb5y(i)/1000,theta1];
     Ei=[diag(xk),pwm,eye(3)];
     
@@ -56,7 +56,7 @@ for i=21:2:size(data,1)-100
 end
 
 format long
-P=lsqminnorm(Input,Output,1e-20,'warn')
+P=lsqminnorm(Input,Output,1e-6,'warn')
 PP=Input\Output;
 
  norm(Input*P-Output)
@@ -69,7 +69,7 @@ PP=Input\Output;
     vector_ca2 = [(data.rb4x(i+1) - data.rb5x(i+1))/1000, (data.rb4y(i+1) - data.rb5y(i+1))/1000];
     theta2 = atan(vector_ca2(2)/vector_ca2(1));
     
-    pwm=[0,data.u2(i),0,-data.u4(i);data.u1(i),0,-data.u3(i),0;data.u1(i),data.u2(i),data.u3(i),data.u4(i)];
+    pwm=[data.u2(i)-data.u4(i),0,0;0,data.u1(i)-data.u3(i),0;0,0,data.u1(i)+data.u2(i)+data.u3(i)+data.u4(i)];
     xk=[data.rb5x(i)/1000,data.rb5y(i)/1000,theta1];
     Ei=[diag(xk),pwm,eye(3)];
     
