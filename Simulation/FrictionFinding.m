be927148b25c465ba2cffa2d905fe681ff9c399c
dyerbm <dyerb@uoguelph.ca>
2020-01-24 10:22:46 -0500

    made some images

--- Simulation/FrictionFinding.m
+++ Simulation/FrictionFinding.m
@@ -1,20 +1,28 @@
+clear all
+clc
+close all
 
 results = []
 Velocity = []
 
 data=[];
-%rawDataPath = "C:\Users\jdiro\Desktop\Git\Omniwheel VICON Code\Raw Data\spinData\";
+rawDataPath = "C:\Users\jdiro\Desktop\Git\Omniwheel VICON Code\Raw Data\50Hz\";
 %data=getData(data,rawDataPath+int2str(i)+"spin_raw.xlsx","A1400:U4000");
-data=getData(data,"C:\Users\jdiro\Desktop\Git\Omniwheel VICON Code\Raw Data\"+"spinNeg150_raw.xlsx","A500:U1000");
-data=getData(data,"C:\Users\jdiro\Desktop\Git\Omniwheel VICON Code\Raw Data\"+"spin150_raw.xlsx","A500:U1000");
-data=getData(data,"C:\Users\jdiro\Desktop\Git\Omniwheel VICON Code\Raw Data\"+"2019-12-10_circ10s_neg160pwm_raw.xlsx","A500:U1700");
-data=getData(data,"C:\Users\jdiro\Desktop\Git\Omniwheel VICON Code\Raw Data\"+"2019-12-10_circ10s_160pwm_raw.xlsx","A500:U1700");
-data=getData(data,"C:\Users\jdiro\Desktop\Git\Omniwheel VICON Code\Raw Data\"+"250circ2_raw.xlsx","A200:U3700");
+%data=getData(data,"C:\Users\jdiro\Desktop\Git\Omniwheel VICON Code\Raw Data\"+"spinNeg150_raw.xlsx","A500:U1000");
+%data=getData(data,"C:\Users\jdiro\Desktop\Git\Omniwheel VICON Code\Raw Data\"+"spin150_raw.xlsx","A500:U1000");
+%data=getData(data,"C:\Users\jdiro\Desktop\Git\Omniwheel VICON Code\Raw Data\"+"2019-12-10_circ10s_neg160pwm_raw.xlsx","A500:U2200");
+%data=getData(data,"C:\Users\jdiro\Desktop\Git\Omniwheel VICON Code\Raw Data\"+"2019-12-10_circ10s_160pwm_raw.xlsx","A500:U1000");
+%data=getData(data,"C:\Users\jdiro\Desktop\Git\Omniwheel VICON Code\Raw Data\"+"250circ2_raw.xlsx","A200:U3700");
+%data=getData(data,"C:\Users\jdiro\Desktop\Git\Omniwheel VICON Code\Raw Data\circspinData\"+"90spin160circle10seconds.xlsx","A100:U400");
+
+data=getData(data,rawDataPath+"200negCirc.xlsx","A50:U2500");
+data=getData(data,rawDataPath+"200Circ.xlsx","A50:U2500");
+data=getData(data,rawDataPath+"200varspin.xlsx","A50:U5000");
 
 %Find Friction
 maxVel = 0.5;
-binSize=0.1;
-numBins = (maxVel*2/binSize)+1;
+binSize=0.01;
+numBins = int16(maxVel*2/binSize)+1;
 vels=linspace(-1*maxVel,maxVel,numBins);
 
 xInput={};
@@ -30,7 +38,7 @@ yOutput{numBins+1}=0;
 xFrictions = zeros(numBins,1);
 yFrictions = zeros(numBins,1);
 
-for i=20:2:size(data,1)-100
+for i=20:1:size(data,1)-1
     
     %calculate current position
     vector_ca1 = [(data.rb4x(i) - data.rb5x(i))/1000, (data.rb4y(i) - data.rb5y(i))/1000];
@@ -49,6 +57,8 @@ for i=20:2:size(data,1)-100
     RotationMatrix = [cos(theta1) -sin(theta1); sin(theta1) cos(theta1)];
     robotXOrientation = [cos(pi/4) sin(pi/4)]*RotationMatrix;
     robotYOrientation = [cos(3*pi/4) sin(3*pi/4)]*RotationMatrix;
+    %robotXOrientation = [1 0]*RotationMatrix;
+    %robotYOrientation = [0 1]*RotationMatrix;
     robotxvel = dot(robotvel,robotXOrientation)/norm(robotXOrientation);
     robotyvel = dot(robotvel,robotYOrientation)/norm(robotYOrientation);
     
@@ -58,11 +68,11 @@ for i=20:2:size(data,1)-100
     
     %append matrices to input and outputs
     
-    if 0<xbin && xbin<numBins
+    if 0<xbin && xbin<=numBins
         xInput{xbin}=[xInput{xbin}; Ei];
         xOutput{xbin}=[xOutput{xbin};data.rb5x(i+1)/1000;data.rb5y(i+1)/1000;theta2];
     end
-    if 0<ybin && ybin<numBins
+    if 0<ybin && ybin<=numBins
         yInput{ybin}=[yInput{ybin};Ei];
         yOutput{ybin}=[yOutput{ybin};data.rb5x(i+1)/1000;data.rb5y(i+1)/1000;theta2];
     end
@@ -84,6 +94,11 @@ end
 %results=[results; P(9)];
 %Velocity = [Velocity; mean(vels)];
 
+
+figure;scatter(vels(1:numBins),yFrictions(1:numBins));ylabel("f_2^d");xlabel("normal velocity (m/s)")
+
+
+figure;scatter(vels(1:numBins),xFrictions(1:numBins));ylabel("f_1^d");xlabel("velocity (m/s)")
 
 
 
--- Simulation/Variable_Finding.m
+++ Simulation/Variable_Finding.m
@@ -56,7 +56,7 @@
 
 %% Read in data
 data=[];
-rawDataPath = "C:\Users\jdiro\Desktop\Git\Omniwheel VICON Code\Raw Data\50Hz"
+rawDataPath = "C:\Users\jdiro\Desktop\Git\Omniwheel VICON Code\Raw Data\50Hz\"
 %data=getData(data,rawDataPath+"spin150_raw.xlsx","A500:U1000");
 %data=getData(data,rawDataPath+"spinNeg150_raw.xlsx","A500:U1000");
 %data=getData(data,rawDataPath+"2019-12-10_circ10s_neg160pwm_raw.xlsx","A100:U2100");
