0137028a001d39fc1fd171c18137cb32e89ea991
dyerbm <dyerb@uoguelph.ca>
2020-04-03 18:10:51 -0400

    Fixed ISMC simulation, now working for tracking straight lines

--- Simulation/SlidingModeControllerSim.m
+++ Simulation/SlidingModeControllerSim.m
@@ -12,6 +12,7 @@ p = 4; % Number of Inputs
 x = zeros(n, length(t)); % Initializes states to zero
 x_d = x; %Initializes desired x states
 z = zeros(m, length(t)); % Initializes measurements to zero
+e = x; %set up error term
 u = zeros(p, length(t)); % Initializes controller
 vs = zeros(p,length(t)); %initialize slip values
 
@@ -19,33 +20,39 @@ rr=0.195; %define robot radius
 wr = 0.03175; %define wheel radius
 
 %A = [-cos(z(3:1)]; % System matrix
-B = @(y)(sqrt(2)/2)*[cos(y(3))+sin(y(3)) -cos(y(3))+sin(y(3)) rr;
-    cos(y(3))-sin(y(3)) cos(y(3))+sin(y(3)) rr;
-    -cos(y(3))-sin(y(3)) cos(y(3))-sin(y(3)) rr;
-    -cos(y(3))+sin(y(3)) -cos(y(3))-sin(y(3)) rr]; % Input matrix
-Binv = @(y)(sqrt(2)/4)*[cos(y(3))-sin(y(3)) cos(y(3))+sin(y(3)) -cos(y(3))+sin(y(3)) -cos(y(3))-sin(y(3));
-    -cos(y(3))-sin(y(3)) cos(y(3))-sin(y(3)) cos(y(3))+sin(y(3)) -cos(y(3))+sin(y(3));
-    1/(sqrt(2)*rr) 1/(sqrt(2)*rr) 1/(sqrt(2)*rr) 1/(sqrt(2)*rr)];
-Rot = @(y)[cos(y(3)) sin(y(3)) 0;
-    -sin(y(3)) cos(y(3)) 0;
-    0 0 1];
+% B = @(y)(sqrt(2)/2)*[cos(y(3))+sin(y(3)) -cos(y(3))+sin(y(3)) rr;
+%     cos(y(3))-sin(y(3)) cos(y(3))+sin(y(3)) rr;
+%     -cos(y(3))-sin(y(3)) cos(y(3))-sin(y(3)) rr;
+%     -cos(y(3))+sin(y(3)) -cos(y(3))-sin(y(3)) rr]; % Input matrix
+% Binv = @(y)(sqrt(2)/4)*[cos(y(3))-sin(y(3)) cos(y(3))+sin(y(3)) -cos(y(3))+sin(y(3)) -cos(y(3))-sin(y(3));
+%     -cos(y(3))-sin(y(3)) cos(y(3))-sin(y(3)) cos(y(3))+sin(y(3)) -cos(y(3))+sin(y(3));
+%     1/(sqrt(2)*rr) 1/(sqrt(2)*rr) 1/(sqrt(2)*rr) 1/(sqrt(2)*rr)];
+Binv = @(y)[(-cos(y(3))-sin(y(3)))/(2*sqrt(2)), (-cos(y(3))+sin(y(3)))/(2*sqrt(2)), (cos(y(3))+sin(y(3)))/(2*sqrt(2)), (cos(y(3))-sin(y(3)))/(2*sqrt(2));
+    (cos(y(3))-sin(y(3)))/(2*sqrt(2)), (-cos(y(3))-sin(y(3)))/(2*sqrt(2)), (-cos(y(3))+sin(y(3)))/(2*sqrt(2)), (cos(y(3))+sin(y(3)))/(2*sqrt(2));
+    1/(4*rr), 1/(4*rr), 1/(4*rr), 1/(4*rr)]; % Input matrix
+B = @(y)[-((cos(y(3))+sin(y(3)))*sqrt(2))/(2*(cos(y(3))^2+sin(y(3))^2)),((cos(y(3))-sin(y(3)))*sqrt(2))/(2*(cos(y(3))^2+sin(y(3))^2)), rr;
+    -((cos(y(3))-sin(y(3)))*sqrt(2))/(2*(cos(y(3))^2+sin(y(3))^2)),-((cos(y(3))+sin(y(3)))*sqrt(2))/(2*(cos(y(3))^2+sin(y(3))^2)), rr;
+    ((cos(y(3))+sin(y(3)))*sqrt(2))/(2*(cos(y(3))^2+sin(y(3))^2)),-((cos(y(3))-sin(y(3)))*sqrt(2))/(2*(cos(y(3))^2+sin(y(3))^2)), rr;
+    ((cos(y(3))+sin(y(3)))*sqrt(2))/(2*(cos(y(3))^2+sin(y(3))^2)),((cos(y(3))+sin(y(3)))*sqrt(2))/(2*(cos(y(3))^2+sin(y(3))^2)), rr];
 
 C = eye(m); % Measurement matrix
 
-K= [1  0 1;
-    0  1 1;
-   -1  0 1;
-    0 -1 1]*10;
 
-% K= [8 0 0;
-%    0 8 0;
-%    0 0 1]*10;
+K= [1 0 0;
+   0 1 0;
+   0 0 1]*1e0;
+
+lambda = [5 0 0;
+          0 5 0;
+          0 0 3]*1e1;
+% lambda=5
 
 %% Set initial conditions
-x(:,1)=[7;4;2];
+x(:,1)=[1;1;1];
 
 for k=1:length(t) %Fill in desired path
-    x_d(:,k)=[0;0;0];
+%     x_d(:,k)=[0;0;0];
+     %x_d(:,k)=[k*T*0.5;k*T*0.5;k*T*0.5];
 end
 
 
@@ -53,24 +60,22 @@ end
 %% Simulate dynamics (for loop)
 for k = 1:length(t)-1 % For loop that simulates 1 second
     %u(:,k+1) = [sin(pi*k*T);cos(pi*k*T);-sin(pi*k*T);-cos(pi*k*T)];% calculate controller
-    e = z(:,k)-x_d(:,k); %calculate the error
+    e(:,k) = z(:,k)-x_d(:,k); %calculate the error
+    e(:,k)
     if k>2
-        edot = (z(:,k-1)-z(:,k))/T; %calculate the derivative error term
+        edot = (e(:,k-1)-e(:,k))/T; %calculate the derivative error term
     else
         edot = [0;0;0];
     end
-    %eint = ;%calculate the integral error term
-    
-    if k==1 %catch case for edot
-        edot=[0;0;0];
-    end
+    eint = sum(e,2)*T; %calculate the integral error term
+    s=e(:,k)+lambda*eint; %calculate 
     
     %u(:,k+1) = 1/wr*(B(x(:,k))*edot-vs(:,k))-B(x(:,k))*K*sign(e);
-    u(:,k+1) = 1/wr*(B(x(:,k))*edot-vs(:,k))-10*K*Rot(x(:,k))*sign(e);
+    u(:,k+1) = -1/wr*(B(x(:,k))*lambda*e(:,k)+vs(:,k))-B(x(:,k))*K*sign(s);
     
     %limit maximum velocity
-    if max(abs(u(:,k+1)))>34
-       u(:,k+1)=u(:,k+1)/max(abs(u(:,k+1)))*34;
+    if max(abs(u(:,k+1)))>40
+       u(:,k+1)=u(:,k+1)/max(abs(u(:,k+1)))*40;
        u(:,k+1)
     end
     
@@ -79,7 +84,8 @@ for k = 1:length(t)-1 % For loop that simulates 1 second
     z(:,k+1) = C*x(:,k+1); % Linear measurement equation
 end
 %% Results
-figure; plot(t, x(1,:)); xlabel('Time (sec)'); % Plots position with time
-figure; plot(t, x(2,:)); xlabel('Time (sec)'); % Plots velocity with time
-figure; plot(t, x(3,:)); xlabel('Time (sec)'); % Plots acceleration with time
+figure; plot(t, x(1,:));hold on;plot(t, x_d(1,:)); xlabel('Time (sec)');hold off; % Plots position with time
+figure; plot(t, x(2,:));hold on;plot(t, x_d(2,:)); xlabel('Time (sec)');hold off; % Plots velocity with time
+figure; plot(t, x(3,:));hold on;plot(t, x_d(3,:)); xlabel('Time (sec)');hold off; % Plots acceleration with time
 figure; plot(t, u); xlabel('Time (sec)'); ylabel('Input'); % Plots input with time
+figure; plot(x(1,:), x(2,:)); xlabel('Time (sec)'); % Plots acceleration with time
