ea01caee792d728fd41cc0a187455b7453493242
dyerbm <dyerb@uoguelph.ca>
2020-04-22 03:06:34 -0400

    Fixed up the Motor PID

--- Simulation/MotorPIDSim.m
+++ Simulation/MotorPIDSim.m
@@ -3,7 +3,7 @@ close all; % Close all figures and windows
 clear; % Clear workspace
 clc; % Clears screen
 %% Initializing parameters
-tf = 0.1; % Final time in simulation
+tf = 1; % Final time in simulation
 T = 2e-5; % Sample rate
 t = 0:T:tf; % Time vector
 
@@ -11,7 +11,7 @@ n_m = 2; % Number of states
 m_m = 1; % Number of measurements
 p_m = 1; % Number of inputs
 num_m=4; %Number of Motors
-J = 0.001;
+J = 0.01;
 b = 1;
 K_m = 1;
 R = 0.6;
@@ -33,9 +33,9 @@ e_m= z_m;
 e_d_m=0;
 eint_m=0;
 
-K_p=-1;
-K_d=1e20;
-K_i=0;
+K_p=-100;
+K_d=-1;
+K_i=-100;
 
 %% Set initial conditions
 
@@ -43,7 +43,7 @@ for k=1:length(t) %Fill in desired path
      x_d(:,k)=[1;1;1;1]; %stabilize to the origin
 end
 
-x(:,1)=[1;1;pi]; %any initial condition
+x(:,1)=[0;0;0;0]; %any initial condition
 %x(:,1)=x_d(:,1); %start at the proper position
 
 z(:,1)=x(:,1); %set first measurement
@@ -53,24 +53,28 @@ z(:,1)=x(:,1); %set first measurement
 for k = 1:length(t)-1 % For loop that simulates 1 second
     
     
-%     e_m(:,k)=z_m(:,k)-x_d(:,k);
-%     eint_m=sum(e_m,2)*T;
-%     if k>2/T %calculate integral error term over previous 2 seconds
-%         eint_m=sum(e_m(:,k-2/T:k),2);
-%     end
-%     if k~=1
-%        e_d_m = (e_m(:,k)-e_m(:,k-1))/T;
-%     end
-%     u_m(:,k+1)=K_p*e_m(:,k)+K_d*e_d_m+K_i*eint_m;%calculate voltages for each motor
+    e_m(:,k)=z_m(:,k)-x_d(:,k);
+    eint_m=sum(e_m,2)*T;
+    if k>2/T %calculate integral error term over previous 2 seconds
+        eint_m=sum(e_m(:,k-2/T:k),2);
+    end
+    if k~=1
+       e_d_m = (e_m(:,k)-e_m(:,k-1))/T;
+    end
+    u_m(:,k+1)=K_p*e_m(:,k)+K_d*e_d_m+K_i*eint_m;%calculate voltages for each motor
+    if abs(max(u_m(:,k+1)))>12
+       u_m(:,k+1)= u_m(:,k+1)/abs(max(u_m(:,k+1)))*12;
+       k
+    end
     
-    u_m(:,k+1)=[12;12;12;12];%force linear controller
+    %u_m(:,k+1)=[12;12;12;12];%force linear controller
     
     u_m(:,k+1);
     
     x_m(:,k+1)=A_m*x_m(:,k)+B_m*u_m(:,k+1);
     z_m(:,k+1)=C_m*x_m(:,k+1);
     
-    x_m(:,k+1)
+    x_m(:,k+1);
 end
 
 
